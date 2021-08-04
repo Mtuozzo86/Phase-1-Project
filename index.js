715712e81a79a5f6da023be79db9719048f19735
Mtuozzo86 <mtuozzo86@gmail.com>
2021-08-04 15:48:54 -0400

    stuff

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const sections = document.querySelector('section')
-const boxArt = document.querySelector('.box-art')
+const sections = document.querySelectorAll('section')
+const boxArt = document.querySelectorAll('.box-art')
 const title = document.querySelector('h1')
 
+boxArt.forEach(item => item.addEventListener('mouseover', function(){
+    item.style.height = "350px"
+    item.style.width = "250px"
+}))
 
-
+boxArt.forEach(item => item.addEventListener('mouseout', function(){
+    item.style.height = "300px"
+    item.style.width = "200px"
+}))
