1ad06d5c1f84793c964454aa8228c81e7e4da357
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-08-05 00:15:38 -0400

    work

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const sections = document.querySelectorAll('section')
 const boxArt = document.querySelectorAll('.box-art')
-const title = document.querySelector('h1')
+const boxArtWhite = document.querySelectorAll('.box-art-white')
 
 boxArt.forEach(item => item.addEventListener('mouseover', function(){
-    item.style.height = "350px"
-    item.style.width = "250px"
+    item.style.boxShadow = '0px 0px 15px 10px #c4c3c3';
+    item.style.transition = 'all 0.3s ease-in-out'
+
 }))
 
 boxArt.forEach(item => item.addEventListener('mouseout', function(){
-    item.style.height = "300px"
-    item.style.width = "200px"
+    item.style.boxShadow = '';
+}))
+
+boxArtWhite.forEach(item => item.addEventListener('mouseover', function(){
+    item.style.boxShadow = '0px 0px 15px 10px #000';
+    item.style.transition = 'all 0.3s ease-in-out'
+    
+}))
+
+boxArtWhite.forEach(item => item.addEventListener('mouseout', function(){
+    item.style.boxShadow = '';
 }))
