016567c2256876012a63693c83b65e9416f06d01
Mtuozzo86 <mtuozzo86@gmail.com>
2021-08-05 23:55:08 -0400

    not much

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const sections = document.querySelectorAll('section')
+const sections = document.querySelector('section')
 const boxArt = document.querySelectorAll('.box-art')
 const boxArtWhite = document.querySelectorAll('.box-art-white')
+const sectionWhite = document.querySelector('.sectionWhite')
 
 boxArt.forEach(item => item.addEventListener('mouseover', function(){
+    
     item.style.boxShadow = '0px 0px 15px 10px #c4c3c3';
     item.style.transition = 'all 0.3s ease-in-out'
 
@@ -29,15 +31,17 @@ fetch("https://swapi.py4e.com/api/films/")
     allFilms.pop()
     allFilms.forEach(elem => console.log(elem.title))
 
-    renderDirector(allFilms)
-})
-
-function renderDirector(info) {
+    function renderDirector(info) {
     
     info.forEach(info => {
-        const director = document.createElement('p')
-        director.innerHTML = info.director;
-        sections.append('director')
+        
+        const dir = document.querySelector('.director')
+        dir.innerHTML = info.director;
+        sections.append(dir)
+        
     })
 
 }
+    renderDirector(allFilms)
+})
+
